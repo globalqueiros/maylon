88ec3e5146de52d20af7e44e0249dac19901470a
globalqueiros <devolpers@groupqueiros.com>
2026-08-11 20:43:06 -0300

    Fist Commit

--- src/lib/vlibras.tsx
+++ src/lib/vlibras.tsx
@@ -1,25 +1,37 @@
 "use client";
-import { useEffect } from "react";
+
 import Script from "next/script";
 
 export default function VLibras() {
-  useEffect(() => {
-    if (window.VLibras) {
-      new window.VLibras.Widget("https://vlibras.gov.br/app");
-    }
-  }, []);
-
   return (
     <>
-      <div vw="true" className="enabled">
-        <div vw-access-button="true" className="active"></div>
-        <div vw-plugin-wrapper="true">
+      <div
+        {...({ vw: "true" } as React.HTMLAttributes<HTMLDivElement>)}
+        className="enabled"
+      >
+        <div
+          {...({ "vw-access-button": "true" } as React.HTMLAttributes<HTMLDivElement>)}
+          className="active"
+        ></div>
+
+        <div
+          {...({ "vw-plugin-wrapper": "true" } as React.HTMLAttributes<HTMLDivElement>)}
+        >
           <div className="vw-plugin-top-wrapper"></div>
         </div>
       </div>
+
       <Script
         src="https://vlibras.gov.br/app/vlibras-plugin.js"
         strategy="afterInteractive"
+        onLoad={() => {
+          if (typeof window !== "undefined" && "VLibras" in window) {
+            // @ts-expect-error VLibras é injetado pelo script externo
+            new window.VLibras.Widget(
+              "https://vlibras.gov.br/app"
+            );
+          }
+        }}
       />
     </>
   );
